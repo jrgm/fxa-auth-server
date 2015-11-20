8b955e7c187435335fda4b008588a837d9865580
John Morrison <jrgmorrison@gmail.com>
2015-11-20 15:00:21 -0800

    check

--- scripts/start-travis-auth-db-mysql.sh
+++ scripts/start-travis-auth-db-mysql.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+set -ev
+
 node ./scripts/gen_keys.js
 node ./node_modules/fxa-auth-db-mysql/bin/db_patcher.js
 node ./node_modules/fxa-auth-db-mysql/bin/server.js & 
+
+# give auth-db-mysql a moment to start up
 sleep 5
