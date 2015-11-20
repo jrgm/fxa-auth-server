175883bb688704a433f3e6b8b6b9d717ac6d2308
John Morrison <jrgmorrison@gmail.com>
2015-11-19 23:24:08 -0800

    check

--- scripts/start-local-test-mysql.sh
+++ scripts/start-local-test-mysql.sh
@@ -2,6 +2,5 @@
 
 node ./scripts/gen_keys.js
 
-ls ./node_modules/fxa-auth-db-mysql/bin/db_patcher.js || npm i ./node_modules/fxa-auth-db-mysql
 node ./node_modules/fxa-auth-db-mysql/bin/db_patcher.js
 node ./node_modules/fxa-auth-db-mysql/bin/server.js
