841bd70659a201b46a6feaa5cffbb76371cc1494
John Morrison <jrgmorrison@gmail.com>
2015-11-20 15:13:50 -0800

    check

--- scripts/start-travis-auth-db-mysql.sh
+++ scripts/start-travis-auth-db-mysql.sh
@@ -3,8 +3,16 @@
 set -ev
 
 node ./scripts/gen_keys.js
+
+# force install of mysql-patcher
+(cd node_modules/fxa-auth-db-mysql && npm install)
+
 node ./node_modules/fxa-auth-db-mysql/bin/db_patcher.js
 node ./node_modules/fxa-auth-db-mysql/bin/server.js & 
 
 # give auth-db-mysql a moment to start up
 sleep 5
+
+# this will cause a test fail if no response
+# TODO: when available, check that implementation is 'MySql'
+curl http://127.0.0.1:8000/
