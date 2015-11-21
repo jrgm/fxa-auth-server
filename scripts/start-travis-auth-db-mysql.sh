f3cff325474cec271d0a4480d393d347d01f7258
John Morrison <jrgmorrison@gmail.com>
2015-11-20 16:02:51 -0800

    check

--- scripts/start-travis-auth-db-mysql.sh
+++ scripts/start-travis-auth-db-mysql.sh
@@ -13,8 +13,8 @@ node ./node_modules/fxa-auth-db-mysql/bin/db_patcher.js
 nohup node ./node_modules/fxa-auth-db-mysql/bin/server.js &>>/var/tmp/db-mysql.out & 
 
 # give auth-db-mysql a moment to start up
-sleep 5
+sleep 10
 
 # this will cause a test fail if no response
 # TODO: when available, check that implementation is 'MySql'
-curl http://127.0.0.1:8000/
+curl -s -v http://127.0.0.1:8000/
